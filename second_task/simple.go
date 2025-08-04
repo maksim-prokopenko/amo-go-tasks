c9fd8f9daa618cfcd35e8d3e9ff8a4767d4607ed
maksim-prokopenko <maksim.prokopenko10@ya.ru>
2025-08-04 20:10:25 +0300

    Update simple.go

--- second_task/simple.go
+++ second_task/simple.go
@@ -1,1 +1,32 @@
+package main
+
+import "sync"
+
+func merge(ch1, ch2 <-chan int) <-chan int {
+	out := make(chan int)
+
+	var wg sync.WaitGroup
+	wg.Add(2)
+
+	go func() {
+		wg.Wait()
+		close(out)
+	}()
+
+	go func() {
+		defer wg.Done()
+		for v := range ch1 {
+			out <- v
+		}
+	}()
+
+	go func() {
+		defer wg.Done()
+		for v := range ch2 {
+			out <- v
+		}
+	}()
+
+	return out
+}
 
